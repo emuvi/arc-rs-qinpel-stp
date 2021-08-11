db2e66cd4e3e0b920f8051a3a08cc08cb6b86000
Éverton M. Vieira <everton.muvi@gmail.com>
2021-08-11 17:34:05 -0300

    prototype development

--- src/boss.rs
+++ src/boss.rs
@@ -1,95 +1,127 @@
 extern crate url;
+extern crate simple_error;
 
 use super::files;
-use std::path::Path;
+use super::utils;
+use std::fs;
 use std::path::PathBuf;
 use url::Url;
-use std::fs;
 
-use super::Result;
+
 use super::data::IndexStp;
+use super::Result;
 
 const ATTEMPTS: usize = 3;
 
 pub struct Boss {
-  url: Url,
-  destiny: PathBuf,
-  temp: PathBuf,
-  placed: Vec<String>,
+    url: Url,
+    destiny: PathBuf,
+    temp: PathBuf,
+    destiny_parent: PathBuf,
+    destiny_backup: PathBuf,
 }
 
 impl Boss {
-  pub fn new(url: Url, destiny: PathBuf, temp: PathBuf) -> Boss {
-    Boss { url, destiny, temp, placed: Vec::new() }
-  }
-
-  pub fn run(&self) {
-    println!("URL Path     : {}", self.url);
-    println!("Destiny Path : {}", self.destiny.display());
-    println!("Temp Path    : {}", self.temp.display());
-    self.start_temp();
-    match self.get_index_stp() {
-      Ok(index_stp) => self.setup(index_stp),
-      Err(e) => println!("{}", e),
+    pub fn new(url: Url, destiny: PathBuf, temp: PathBuf) -> Boss {
+        let destiny_name = destiny
+            .file_name()
+            .expect("error: could not get the name of the destiny")
+            .to_str()
+            .expect("error: could not convert to str the file name of the destiny");
+        let destiny_parent = destiny
+            .parent()
+            .expect("error: could not get the parent of the destiny")
+            .to_owned();
+        let mut destiny_backup = destiny_name.to_owned();
+        destiny_backup.push_str("_bkp");
+        let destiny_backup = destiny_parent.join(destiny_backup);
+        Boss {
+            url,
+            destiny,
+            temp,
+            destiny_parent,
+            destiny_backup,
+        }
     }
-    self.finish();
-  }
 
-  fn start_temp(&self) {
-    if self.temp.exists() {
-      panic!("There's already a temp path. This implies that another instance of this program is running or that was an crash in some previous execution. You must check-it.");
+    pub fn run(&self) {
+        self.start_temp();
+        match self.get_index_stp() {
+            Ok(index_stp) => {
+                match self.setup(index_stp) {
+                    Err(e) => println!("{}", e),
+                    _ => (),
+                }
+            },
+            Err(e) => println!("{}", e),
+        }
+        self.finish();
     }
-    fs::create_dir_all(&self.temp).expect("Could not create the temp path.");
-  }
 
-  fn get_index_stp(&self) -> Result<IndexStp> {
-    let url = self.url.join("index-stp.json")?;
-    let body = get_text(url)?;
-    Ok(IndexStp::new(body)?)
-  }
+    fn start_temp(&self) {
+        if self.temp.exists() {
+            panic!("error: there's already a temp path");
+        }
+        fs::create_dir_all(&self.temp).expect("error: could not create the temp path");
+    }
 
-  fn setup(&self, index_stp: IndexStp) {
-    println!("IndexStp {:?}", index_stp);
-    let mut ancestors = self.destiny.ancestors();
-    println!("{:?}", ancestors.next());
-    println!("{:?}", ancestors.next());
-  }
+    fn get_index_stp(&self) -> Result<IndexStp> {
+        let url = self.url.join("index-stp.json")?;
+        let body = get_text(url)?;
+        Ok(IndexStp::new(body)?)
+    }
 
-  fn finish(&self) {
-    fs::remove_dir_all(&self.temp).expect("Could not delete the temp path.");
-  }
+    fn setup(&self, index_stp: IndexStp) -> Result<()> {
+        self.backup_actual()?;
+        Ok(())
+    }
+
+    fn backup_actual(&self) -> Result<()> {
+        if self.destiny_backup.exists() {
+            return Err(utils::get_err("// TODO"))
+        }
+        Ok(())
+    }
+
+    fn restore_actual(&self) -> Result<()> {
+        Ok(())
+    }
+
+    fn finish(&self) {
+        fs::remove_dir_all(&self.temp).expect("error: could not delete the temp path");
+    }
 }
 
 fn get_text(url: Url) -> Result<String> {
-  let mut attempt = 1;
-  loop {
-    println!("Getting text attempt {} from {}", attempt, url);
-    match files::download_text(url.as_ref()) {
-      Ok(text) => return Ok(text),
-      Err(e) => {
-        println!("{}", e);
-        if attempt > ATTEMPTS {
-          return Err(e)
+    let mut attempt = 1;
+    loop {
+        println!("getting text attempt {} from {}", attempt, url);
+        match files::download_text(url.as_ref()) {
+            Ok(text) => return Ok(text),
+            Err(e) => {
+                println!("{}", e);
+                if attempt > ATTEMPTS {
+                    return Err(e);
+                }
+            }
         }
-      },
+        attempt += 1;
     }
-    attempt += 1; 
-  }
 }
 
 fn get_file(url: Url, file: String) -> Result<()> {
-  let mut attempt = 1;
-  loop {
-    println!("Getting file attempt {} from {} to {}", attempt, url, file);
-    match files::download_file(url.as_ref(), &file) {
-      Ok(()) => return Ok(()),
-      Err(e) => {
-        println!("{}", e);
-        if attempt > ATTEMPTS {
-          return Err(e)
+    let mut attempt = 1;
+    loop {
+        println!("getting file attempt {} from {} to {}", attempt, url, file);
+        match files::download_file(url.as_ref(), &file) {
+            Ok(()) => return Ok(()),
+            Err(e) => {
+                println!("{}", e);
+                if attempt > ATTEMPTS {
+                    return Err(e);
+                }
+            }
         }
-      },
+        attempt += 1;
     }
-    attempt += 1;
-  }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,13 @@ const SEPARATOR: char = std::path::MAIN_SEPARATOR;
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn setup(url_root: String, file_root: String) {
-    let url_path = url::Url::parse(&url_root).expect("Could not parse the url root.");
-    let current_dir = std::env::current_dir().expect("Could not retrieve the current dir.");
+    let url_path = url::Url::parse(&url_root).expect("error: could not parse the url root");
+    let current_dir = std::env::current_dir().expect("error: could not retrieve the current dir");
     let destiny_path = current_dir.join(&file_root);
     let temp_path = current_dir.join("tmp");
+    println!("setup url path: {}", url_path);
+    println!("setup destiny path: {}", destiny_path.display());
+    println!("setup temp path: {}", temp_path.display());
     boss::Boss::new(url_path, destiny_path, temp_path).run();
 }
 
@@ -34,32 +37,32 @@ fn setup_cmd(os: &str, arch: &str, name: &str) {
 }
 
 fn main() {
-    println!("Qinpel Setup starting...");
+    println!("Qinpel setup starting...");
     let os = utils::get_os();
     let arch = utils::get_arch();
-    println!("Identified operation system: {}", os);
-    println!("Identified system architecture: {}", arch);
+    println!("identified operation system: {}", os);
+    println!("identified system architecture: {}", arch);
     for (index, argument) in std::env::args().enumerate() {
         if index == 0 {
             continue;
         }
         if argument.len() < 6 {
             println!(
-                "Error: Can not setup this mal formed argument: {}.",
+                "error: can not setup this very small argument: {}",
                 argument
             );
             continue;
         }
         let name = &argument[5..];
         if argument.starts_with("apps/") {
-            println!("Setting up application: {}.", name);
+            println!("setting up application: {}", name);
             setup_app(name);
         } else if argument.starts_with("cmds/") {
-            println!("Setting up command: {}.", name);
+            println!("setting up command: {}", name);
             setup_cmd(os, arch, name);
         } else {
             println!(
-                "Error: Can not setup this mal formed argument: {}.",
+                "error: can not setup this mal formed argument: {}",
                 argument
             );
         }
